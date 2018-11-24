228e7d1e5e373e25ace8a7157184e37b2bc00661
Michael Burnley <micheal.burnley@v1.co>
2018-11-23 18:40:26 -0800

    updating procfile

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const Discord = require('discord.js')
 const DOTENV = require('dotenv').config();
+const http = require ('http');
 const client = new Discord.Client();
+const handle = (req, res) => {res.send("hit")}
+const server = http.createServer(handle);
+
+server.listen(process.env.PORT || 5000);
 
 client.on('message', msg => {
-    if (msg.content === "hi coffeebot") {
+    if(msg.author === "killerjose11" || msg.author === "plasmonster") {
+        msg.reply("Look, y'all need to stop.")
+    }
+    else if (msg.content === "hi coffeebot" || msg.content === "hey coffeebot" || msg.content === "coffeebot" || msg.content === "whats up coffebot") {
         msg.reply('Hey! I\'m a bot.')
     }
 })
